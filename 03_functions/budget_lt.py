ed9de8eddf90aa8130e62e4c0a3dc386ca9adf42
vokator-online <vokator@gmail.com>
2024-01-04 15:25:48 +0200

    CD komitas

--- 03_functions/budget_lt.py
+++ 03_functions/budget_lt.py
@@ -11,3 +11,29 @@ Reikalavimai
 * Suskaičiuoti biudžeto balansą
 
 """
+biudzetas = {}
+
+def prideti_pajamas_isskaiciuoti_balansa(paskirtis, suma):
+    if paskirtis not in biudzetas:
+        biudzetas[paskirtis] = 0
+    biudzetas[paskirtis] += suma
+
+def prideti_islaidas_isskaiciuoti_balansa(paskirtis, suma):
+    prideti_pajamas_isskaiciuoti_balansa(paskirtis, -suma)
+
+def spausdinti_zurnala():
+    print("Biudžeto turinys:")
+    for paskirtis, suma in biudzetas.items():
+        print(f"{paskirtis}: {suma}")
+
+def suskaiciuoti_balansa():
+    balansas = sum(biudzetas.values())
+    print(f"Biudžeto balansas: {balansas}")
+
+# Pavyzdinė naudojimo situacija:
+prideti_pajamas_isskaiciuoti_balansa("Atlyginimas", 2000)
+prideti_islaidas_isskaiciuoti_balansa("Maistas", 300)
+prideti_islaidas_isskaiciuoti_balansa("Kuras", 200)
+prideti_islaidas_isskaiciuoti_balansa("Pramogos", 200)
+spausdinti_zurnala()
+suskaiciuoti_balansa()
